6a85d461df0e92ab001081facc8e759af04d23e6
Pranav N <pranavbharadwaj142@gmail.com>
2022-05-02 12:21:18 +0530

    Update auction.sol

--- Auction-contract/auction.sol
+++ Auction-contract/auction.sol
@@ -11,7 +11,6 @@ contract Auction{
    enum Auc_State {Started,Running,Ended,Cancelled}
    Auc_State public auctionState;
 
-   uint public highestBid;
    uint public highestPayableBid;
    uint bidInc;
 
@@ -48,8 +47,60 @@ contract Auction{
      auctionState=Auc_State.Cancelled;
    }
 
+    function endAuc() public Owner{
+     auctionState=Auc_State.Ended;
+   }
+
+    function min(uint a,uint b) pure private returns(uint){
+            if(a<=b){
+                return a;
+            }else{
+                return b;
+            }
+    }
+
    function bid() payable public notOwner Started beforeEnding{
         require(auctionState==Auc_State.Running);
         require(msg.value>1 ether);
+
+        uint currentBid=bids[msg.sender]+msg.value;
+        require(currentBid>highestPayableBid);
+
+        bids[msg.sender]=currentBid;
+
+        if(currentBid<bids[highestBidder]){
+            highestPayableBid=min(currentBid+bidInc,bids[highestBidder]);
+        }else{
+            highestPayableBid=min(currentBid,bids[highestBidder]+bidInc);
+            highestBidder=payable(msg.sender);
+        }
    }
+
+    function finiizeAuc() public{
+        require(auctionState==Auc_State.Cancelled || auctionState==Auc_State.Ended || block.number>etblock);
+        require(msg.sender==auctioneer || bids[msg.sender]>0);
+
+        address payable person;
+        uint value;
+
+        if(auctionState==Auc_State.Cancelled){
+            person=payable(msg.sender);
+            value=bids[msg.sender];
+        }else{
+            if(msg.sender==auctioneer){
+                person=auctioneer;
+                value=highestPayableBid;
+            }else{
+                if(msg.sender==highestBidder){
+                    person=highestBidder;
+                    value=bids[highestBidder]-highestPayableBid;
+                }else{
+                    person=payable(msg.sender);
+                    value=bids[msg.sender];
+                }
+            }
+        }
+        bids[msg.sender]=0;
+        person.transfer(value);
+    }
 }
